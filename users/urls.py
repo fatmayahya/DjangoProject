41ac902e81a493e2ccd4f6ff49b1e1bae8e08ddd
fatmayahya <fatmayahyahaiba@gmail.com>
2025-01-20 17:07:26 +0300

    users

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from .views.auth_views import login_view, logout_view
+from .views.profile_views import profile_view
+from django.urls import path 
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('profile/', profile_view, name='profile'),
+]
